e37096d000cc960d5bfa25c31f0b3222fe61815d
Diu1011 <91621863+Diu1011@users.noreply.github.com>
2023-07-16 16:03:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,207 @@
+const container=document.querySelector('.container');
+const questionBox=document.querySelector('.question');
+const choicesBox=document.querySelector('.choices');
+const nextBtn=document.querySelector('.nextBtn');
+const scoreCard=document.querySelector('.scoreCard');
+const alert=document.querySelector('.alert');
+const startBtn=document.querySelector('.startBtn');
+const timer=document.querySelector('.timer');
+
+const quiz= [
+{
+    question:"Q. Which of the following is not CSS box model property?",
+    choices:["margin","padding","border-radius","border-collapse"],
+    answer:"border-collapse"
+},
+{
+    question:"Q. Which function is used to serialize an object into a JSON string in Javascript?",
+    choices:["stringify()","parse()","convert()","None of then above"],
+    answer:"stringify()"
+},
+{
+    question:"Q. Which of the following is not javascript data type?",
+    choices:["String","boolean","object","float"],
+    answer:"float"
+},
+{
+    question:"Q. How can a datatype be declared to be a constant type?",
+    choices:["const","constant","var","let"],
+    answer:"const"
+},
+{
+    question:"Q. When an operator’s value is NULL, the typeof returned by the unary operator is",
+    choices:["boolean","undefined","object","Integer"],
+    answer:"object"
+}
+
+];
+
+let currentQuestionIndex = 0;
+let score = 0;
+let quizOver = false;
+let timeLeft = 15;
+let timerId = null;
+
+const showQuestions = () => {
+    const questionDetails=quiz[currentQuestionIndex];
+    questionBox.textContent=questionDetails.question;
+
+    choicesBox.textContent="";
+    for(let i=0; i<questionDetails.choices.length; i++)
+    {
+        const currentChoice = questionDetails.choices[i];
+        const choiceDiv = document.createElement('div');
+        choiceDiv.textContent = currentChoice;
+        choiceDiv.classList.add('choice');
+        choicesBox.appendChild(choiceDiv);
+        
+        choiceDiv.addEventListener('click', ()=>{
+            if(choiceDiv.classList.contains('selected'))
+            {
+                choiceDiv.classList.remove('selected');
+            }
+            else
+            {
+                choiceDiv.classList.add('selected');
+            }
+
+        });
+    
+}
+
+if(currentQuestionIndex < quiz.length)
+{
+    startTimer();
+}
+
+}
+ 
+
+const checkAnswer = () => {
+    const selectedChoice=document.querySelector('.choice.selected');
+    if(selectedChoice.textContent === quiz[currentQuestionIndex].answer)
+    {
+        displayAlert("Correct Answer!");
+        score++;
+    }
+    else{
+        displayAlert(`Wrong Answer! ${quiz[currentQuestionIndex].answer} is the Correct Answer`);
+
+    }
+    timeLeft=15;
+    currentQuestionIndex++;
+    if(currentQuestionIndex < quiz.length)
+    {
+       
+        showQuestions();
+    }
+    else{
+        stopTimer();
+        showScore();
+        
+        
+    }
+
+}
+
+const showScore = () => {
+    questionBox.textContent = "";
+    choicesBox.textContent = "";
+    scoreCard.textContent = `You scored ${score} out of ${quiz.length}`;
+    displayAlert("You have completed the quiz!!");
+    nextBtn.textContent = "Play Again";
+    quizOver = true;
+    timer.style.display = "none";
+} 
+ const displayAlert = (msg) => {
+    alert.style.display = "block";
+    alert.textContent = msg;
+    setTimeout(() => {
+        alert.style.display = "none";
+        
+
+    }, 2000);
+    
+ }
+
+const startTimer = () => {
+    clearInterval(timerId);
+    timer.textContent = timeLeft;
+    const countDown = () => {
+        timeLeft--;
+        timer.textContent = timeLeft;
+        if(timeLeft === 0){
+            const confirmUser = confirm("Time Up!!! Do you want to play the quiz again?");
+            if(confirmUser){
+                timeLeft = 15;
+                startQuiz();
+            }
+            else{
+                startBtn.style.display = "block";
+                container.style.display = "none";
+                return;
+            }
+
+        }
+
+    }
+   timerId = setInterval(countDown, 1000);
+    
+}
+
+
+const stopTimer = () => {
+    clearInterval(timerId);
+
+
+}
+
+
+const shuffleQuestions = () => {
+    for(let i=quiz.length-1 ; i>0; i--)
+    {
+        const j = Math.floor(Math.random() * (i+1));
+        [quiz[i], quiz[j]] = [quiz[j], quiz[i]];
+    }
+    currentQuestionIndex = 0;
+    showQuestions();
+}
+
+const startQuiz = () => {
+    timeLeft = 15;
+    timer.style.display = "flex";
+    shuffleQuestions();
+}
+
+startBtn.addEventListener('click', ()=> {
+    startBtn.style.display = "none";
+    container.style.display = "block";
+    startQuiz();
+
+});
+
+ nextBtn.addEventListener('click', ()=> {
+    const selectedChoice = document.querySelector('.choice.selected');
+    if(!selectedChoice && nextBtn.textContent === "Next")
+    {
+        displayAlert("Select your answer");
+        return;
+    }
+    if(quizOver)
+    {
+        nextBtn.textContent = "Next";
+        scoreCard.textContent = "";
+        currentQuestionIndex=0;
+        showQuestions();
+        quizOver = false;
+        score = 0;
+        startQuiz();
+
+    }
+    else{
+        checkAnswer();
+    }
+   
+    
+ });
+ 
